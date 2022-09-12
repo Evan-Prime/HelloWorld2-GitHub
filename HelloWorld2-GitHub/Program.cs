ec2bb6d45c7f313c83043ae265fc25c0da6f22e4
Evan MacDonald <evan.t.macdonald01@gmail.com>
2022-09-12 11:47:53 -0300

    Initial commit

---
+++ HelloWorld2-GitHub/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld2_GitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("     +----+     ");
+            Console.WriteLine("     |    |     ");
+            Console.WriteLine("+----+    +----+");
+            Console.WriteLine("| Hello World! |");
+            Console.WriteLine("+----+    +----+");
+            Console.WriteLine("     |    |     ");
+            Console.WriteLine("     +----+     ");
+            Console.WriteLine("");
+            Console.WriteLine("By: Evan MacDonald");
+            Console.WriteLine("");
+            Console.ReadKey(true);
+
+        }
+    }
+}
